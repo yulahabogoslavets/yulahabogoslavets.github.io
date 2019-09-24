3f20cea9deebe49b38c3244107dc42f5903757a0
yulahabogoslavets <speed0435@gmail.com>
2019-09-24 11:20:45 +0200

    block work

--- piroll/js/counter.js
+++ piroll/js/counter.js
@@ -1,25 +1,7 @@
-
-/*function counter(time){
-	let start = 0;
-	let progressElement = document.querySelector('.numbers');
-	let num = progressElement.dataset.num;
-	let intervalId = setInterval(function(){
-		if(start>num){
-			clearInterval(intervalId);
-		}else{
-			progressElement.value = start;
-		}
-		start++;
-	}, time);
-}
-
-counter(100);*/
-
 var number = document.querySelector('.numbers'),
 	numberTop = number.getBoundingClientRect().top,
     start = 0,
-    //end = 300;
-	end = number.dataset.num;
+	end = this.number.dataset.num;
 
 
 window.addEventListener('scroll', function onScroll() {
